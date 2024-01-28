29b6a7f2c08b449814337a53367404412dd45eb5
Alexandra-m22 <157401928+Alexandra-m22@users.noreply.github.com>
2024-01-28 21:11:33 +0300

    Add files via upload

---
+++ Program 1.cs
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    int a, b;
+    cout << "Введите в одной строке два целых числа и нажмите <Enter> ";
+    cin >> a >> b;
+    if (a == b) cout << "числа " << a << " и " << b << " равны";
+    else cout << a << ((a < b) ? " меньше " : " больше ") << b;
+    cout << endl;
+    system("pause");
+    return 0;
