31276a6e5ecb66bfbc69759c8856d66a5704cca5
Alexandra-m22 <157401928+Alexandra-m22@users.noreply.github.com>
2024-01-28 20:59:46 +0300

    Add files via upload

---
+++ Program 2.cs
@@ -0,0 +1,13 @@
+﻿{
+    Console.WriteLine("Введите число: ");
+    int value = Convert.ToInt32(Console.ReadLine());
+
+    int num1 = 5;
+    int num2 = 10;
+
+
+    if (value > num1 && value < num2)
+        Console.WriteLine("Число больше 5 и меньше 10");
+    else
+        Console.WriteLine("Неизвестное число");
+}
